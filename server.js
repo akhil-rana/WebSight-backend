5631842927684dd3b95d17b7ae16f0769881f3da
akhilrana0001 <akhilrana0001@gmail.com>
2019-12-13 23:30:57 +0530

    master

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const puppeteer = require("puppeteer");
 const $ = require("cheerio");
-const cors = require("cors");
+// const cors = require("cors");
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080;
-app.use(cors());
+// app.use(cors());
+// Add headers
 app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://angular-googlesearch-frontend.herokuapp.com"
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
   next();
 });
 var searchKey;
