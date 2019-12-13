64e4fda418a0165a5c4885c2149ad764505da3de
akhilrana0001 <akhilrana0001@gmail.com>
2019-12-13 22:57:31 +0530

    master

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 var searchKey;
 app.get("/gsearch", (req, res) => {
   res.send("Welcome to post Node API");
