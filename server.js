856fa38de629f9b992a378e3cfc517d5ea30f6b4
akhilrana0001 <akhilrana0001@gmail.com>
2019-12-14 03:19:34 +0530

    master

--- server.js
+++ server.js
@@ -22,54 +22,6 @@ app.post("/postData", bodyParser.json(), (req, res) => {
 var server = app.listen(process.env.PORT || 8080, () =>
   console.log("App listening on port " + PORT)
 );
-// var arr4 = [];
-
-// function google_scrape(url, res) {
-//   var arr = [];
-//   var arr1 = [];
-//   var output;
-//   puppeteer
-//     .launch({ args: ["--no-sandbox"] })
-//     .then(function(browser) {
-//       return browser.newPage();
-//     })
-//     .then(function(page) {
-//       return page.goto(url).then(function() {
-//         return page.content();
-//       });
-//     })
-
-//     .then(function(html) {
-//       $(" .LC20lb", html).each(function() {
-//         arr.push(
-//           $(this)
-//             .parent()
-//             .attr("href")
-//         );
-//         arr1.push($(this).text());
-//       });
-//       a = 0;
-//       // $("span.st", html).each(function() {
-//       //   arr4.push($(this).text());
-//       // });
-//     })
-
-//     .then(function() {
-//       // var arr2 = Array.from(new Set(arr));
-//       // var arr3 = Array.from(new Set(arr1));
-//       output = { link: url, titles: arr1, urls: arr };
-//       console.log(output);
-//       res.json(output);
-
-//       // browser.close(); //   process.exit();
-//       // console.log(output);
-//       // return output;
-//     })
-//     .catch(function(err) {
-//       //handle error
-//       console.log(err);
-//     });
-// }
 
 function google_scrape(url, res) {
   (async () => {
@@ -80,6 +32,7 @@ function google_scrape(url, res) {
     });
     var arr = [],
       arr1 = [];
+    arr2 = [];
     var HTML = await page.content();
     $(" .LC20lb", HTML).each(function() {
       arr.push(
@@ -89,13 +42,15 @@ function google_scrape(url, res) {
       );
       arr1.push($(this).text());
     });
-    var output = { link: url, titles: arr1, urls: arr };
-    console.log(output);
+    $(" .s .st", HTML).each(function() {
+      arr2.push($(this).text());
+    });
+    var output = { link: url, titles: arr1, urls: arr, gist: arr2 };
+    // console.log(output);
 
     res.json(output);
 
     browser.close();
     return;
   })();
-  // console.log("hello");
 }
